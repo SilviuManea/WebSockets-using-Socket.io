37439df08360e6ed12459f8aec42e2c0d6912820
Silviu <silviu.manea.hamzau@outlook.com>
2021-11-12 10:11:52 +0100

    3-handling the messages in the client chat side

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,17 @@
 //Make connection using socket (frontend side) - we need to use <script src="/socket.io/socket.io.js"></script> on the index.html in order to do this
 var socket = io.connect('http://localhost:4000');
+
+//Querying the DOM - so we can get the handle of the different elemnts we are interacting with.
+var message = document.getElementById('message');
+    handle = document.getElementById('handle');
+    btn = document.getElementById('send');
+    output = document.getElementById('output');
+
+//Emit Events
+btn.addEventListener('click',function(){
+    socket.emit('chat',{//emits a message down the websocket to the server
+        message:message.value, //we send this values
+        handle:handle.value 
+    }) 
+
+})
